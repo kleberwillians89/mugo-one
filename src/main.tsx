9eae279b7d203e5ca9baa123e2956002aa3a2a0c
kleberwillians89 <kleberw.dev@gmail.com>
2026-08-13 19:45:47 -0300

    chore: add design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthRoot } from './Auth'
+import './styles/tokens.css'
 import './styles.css'
 import './enhancements.css'
 import './auth.css'
